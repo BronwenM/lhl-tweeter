66ff3ee1f678474ca2f75248cf0a75cc7a528de7
BronwenM <bronwen.marchant@gmail.com>
2024-10-11 00:24:01 -0700

    added some design to tweets display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,7 @@
 $(document).ready(function () {
   const noHTML = (str) => {
     const regex = /\<\/?[a-z]*\>/gm;
-  
+
     return str.replaceAll(regex, '')
   }
 
@@ -41,30 +41,30 @@ $(document).ready(function () {
   }
 
   const renderAllTweets = (allTweets) => {
-    for(const tweet of allTweets) {
+    for (const tweet of allTweets) {
       const $singleTweet = createTweet(tweet);
       $('#tweets').prepend($singleTweet);
     }
   }
 
   const loadTweets = () => {
-    $.get('/tweets', function(data) {  
+    $.get('/tweets', function (data) {
       renderAllTweets(data);
     })
-    .fail(function() {
-      console.error('An error occurred while fetching tweets.');
-    });
+      .fail(function () {
+        console.error('An error occurred while fetching tweets.');
+      });
   }
 
   loadTweets();
 
-  $('.tweet-warning button').on('click', function(){
-    $('.tweet-warning').css({opacity: 0});
+  $('.tweet-warning button').on('click', function () {
+    $('.tweet-warning').css({ opacity: 0 });
 
-    setTimeout(function(){ $('.tweet-warning').css({display: "none"}) }, 300);
+    setTimeout(function () { $('.tweet-warning').css({ display: "none" }) }, 300);
   })
 
-  $('#create-tweet').on('submit', function(event) {
+  $('#create-tweet').on('submit', function (event) {
     event.preventDefault();
     const tweetContent = noHTML($(this).find('textarea').val().trim());
     const serializedData = $(this).serialize();
@@ -72,17 +72,17 @@ $(document).ready(function () {
     console.log('tweetContent:', tweetContent)
     console.log('serializedData:', serializedData)
 
-    if(!tweetContent) {
+    if (!tweetContent) {
       $('.tweet-warning span').text('There\'s nothing there!');
-      $('.tweet-warning').css({display: "flex", opacity: 1});
-    } else if( tweetContent.length > 140) {
+      $('.tweet-warning').css({ display: "flex", opacity: 1 });
+    } else if (tweetContent.length > 140) {
       $('.tweet-warning span').text('Your Tweet is too long!');
-      $('.tweet-warning').css({display: "flex", opacity: 1});
+      $('.tweet-warning').css({ display: "flex", opacity: 1 });
     } else {
       $.post('/tweets', serializedData, () => {
         $(this).find('textarea').val('');
-        $('.tweet-warning').css({opacity: 0});
-        setTimeout(function(){ $('.tweet-warning').css({display: "none"}) }, 300);
+        $('.tweet-warning').css({ opacity: 0 });
+        setTimeout(function () { $('.tweet-warning').css({ display: "none" }) }, 300);
         loadTweets();
       });
     }
@@ -92,27 +92,40 @@ $(document).ready(function () {
 
   const openCloseNewTweet = (isHidden) => {
 
-      if(!isHidden) {
-        $('.new-tweet').css({opacity: 1, transform: 'translateY(0)'});
-        $('#tweets').css({transform: 'translateY(0)'})
+    if (!isHidden) {
+      $('.new-tweet').css({ opacity: 1, transform: 'translateY(0)' });
+      $('#tweets').css({ transform: 'translateY(0)' })
+      $('#create-tweet textarea').trigger('focus')
+      $('.nav-cta i').css({ transform: 'rotate(-180deg)' });
+      $('.nav-cta span').html('<b>Browse</b> the timeline');
 
-        $('.nav-cta i').css({transform: 'rotate(0)'});
-        $('.nav-cta span').html('<b>Browse</b> the timeline');
-        newTweetIsHidden = true;
-  
-      } else {
-        $('.new-tweet').css({opacity: 0, transform: 'translateY(-220px)'});
-        $('#tweets').css({transform: 'translateY(-220px)'})
-        $('.nav-cta i').css({transform: 'rotate(-180deg)'});
-        $('.nav-cta span').html('<b>Write</b> a new tweet');
-        newTweetIsHidden = false;
-      }
-    
+      newTweetIsHidden = true;
+
+    } else {
+      $('.new-tweet').css({ opacity: 0, transform: 'translateY(-220px)' });
+      $('#tweets').css({ transform: 'translateY(-220px)' })
+      $('.nav-cta i').css({ transform: 'rotate(0)' });
+      $('.nav-cta span').html('<b>Write</b> a new tweet');
+
+      newTweetIsHidden = false;
+    }
+
   }
-  
+
   openCloseNewTweet(newTweetIsHidden);
 
-  $('.nav-cta').on('click', function(){
-        openCloseNewTweet(newTweetIsHidden);
+  $('.nav-cta').on('click', function () {
+    openCloseNewTweet(newTweetIsHidden);        
+
+    //this works but I find openCloseNewTweet looks better ¯\_(ツ)_/¯
+    /* if (newTweetIsHidden) {
+      $('.new-tweet').slideUp("slow")
+      newTweetIsHidden = false;
+
+    } else {
+      $('.new-tweet').slideDown("slow")
+      $('#create-tweet textarea').trigger('focus')
+      newTweetIsHidden = true;
+    } */
   })
 })
