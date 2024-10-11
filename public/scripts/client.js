63772e02f37f72b2b98fe3205024e36537086e86
BronwenM <bronwen.marchant@gmail.com>
2024-10-11 01:03:35 -0700

    updated README.md

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,12 +4,13 @@
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-$(document).ready(function () {
+//use regex to strip any html tags from the tweet text to prevent XSS
+$(document).ready(function() {
   const noHTML = (str) => {
     const regex = /\<\/?[a-z]*\>/gm;
 
-    return str.replaceAll(regex, '')
-  }
+    return str.replaceAll(regex, '');
+  };
 
   const createTweet = (tweetData) => {
     const timeagoFormatted = timeago.format(tweetData.created_at);
@@ -38,40 +39,47 @@ $(document).ready(function () {
     `);
 
     return $newTweet;
-  }
+  };
 
+  //takes the array of tweets and creates a newTweet obj to put at the top of the tweets section
   const renderAllTweets = (allTweets) => {
     for (const tweet of allTweets) {
       const $singleTweet = createTweet(tweet);
       $('#tweets').prepend($singleTweet);
     }
-  }
+  };
 
+  //calls a get to load all the tweets in /tweets endpoint
   const loadTweets = () => {
-    $.get('/tweets', function (data) {
+    $.get('/tweets', function(data) {
+      //clear the tweets section briefly to avoid repetitions (entire set of tweets getting prepended again)
+      $('#tweets').empty();
       renderAllTweets(data);
     })
-      .fail(function () {
+      .fail(function() {
         console.error('An error occurred while fetching tweets.');
       });
-  }
+  };
 
   loadTweets();
 
-  $('.tweet-warning button').on('click', function () {
+  //handle the click event to close the new tweet warning modal (no content or too long alert)
+  $('.tweet-warning button').on('click', function() {
     $('.tweet-warning').css({ opacity: 0 });
 
-    setTimeout(function () { $('.tweet-warning').css({ display: "none" }) }, 300);
-  })
+    setTimeout(function() {
+      $('.tweet-warning').css({ display: "none" });
+    }, 300);
+  });
 
-  $('#create-tweet').on('submit', function (event) {
+  //create a new tweet on form submission
+  $('#create-tweet').on('submit', function(event) {
     event.preventDefault();
+
     const tweetContent = noHTML($(this).find('textarea').val().trim());
     const serializedData = $(this).serialize();
-    console.log('this:', $(this));
-    console.log('tweetContent:', tweetContent)
-    console.log('serializedData:', serializedData)
 
+    //check that the form field actually has content before allowing it to post
     if (!tweetContent) {
       $('.tweet-warning span').text('There\'s nothing there!');
       $('.tweet-warning').css({ display: "flex", opacity: 1 });
@@ -81,41 +89,49 @@ $(document).ready(function () {
     } else {
       $.post('/tweets', serializedData, () => {
         $(this).find('textarea').val('');
+        $('.tweet-output output').val('140');
+
         $('.tweet-warning').css({ opacity: 0 });
-        setTimeout(function () { $('.tweet-warning').css({ display: "none" }) }, 300);
+        
+        setTimeout(function() {
+          $('.tweet-warning').css({ display: "none" });
+        }, 300);
+
         loadTweets();
       });
     }
-  })
+  });
 
+  //bool is the create tweet section hidden?
   let newTweetIsHidden = true;
 
+  //check if the create tweet section is hidden, then transform the create tweet and tweets sections. also transform the nav-cta
   const openCloseNewTweet = (isHidden) => {
 
     if (!isHidden) {
       $('.new-tweet').css({ opacity: 1, transform: 'translateY(0)' });
-      $('#tweets').css({ transform: 'translateY(0)' })
-      $('#create-tweet textarea').trigger('focus')
+      $('#tweets').css({ transform: 'translateY(0)' });
+      $('#create-tweet textarea').trigger('focus');
       $('.nav-cta i').css({ transform: 'rotate(-180deg)' });
-      $('.nav-cta span').html('<b>Browse</b> the timeline');
+      $('.nav-cta span').html('<b>Just Browse</b> the timeline');
 
       newTweetIsHidden = true;
 
     } else {
       $('.new-tweet').css({ opacity: 0, transform: 'translateY(-220px)' });
-      $('#tweets').css({ transform: 'translateY(-220px)' })
+      $('#tweets').css({ transform: 'translateY(-220px)' });
       $('.nav-cta i').css({ transform: 'rotate(0)' });
       $('.nav-cta span').html('<b>Write</b> a new tweet');
 
       newTweetIsHidden = false;
     }
 
-  }
+  };
 
   openCloseNewTweet(newTweetIsHidden);
 
   $('.nav-cta').on('click', function() {
-    openCloseNewTweet(newTweetIsHidden);        
+    openCloseNewTweet(newTweetIsHidden);
 
     //this works but I find openCloseNewTweet looks better ¯\_(ツ)_/¯
     /* if (newTweetIsHidden) {
@@ -127,5 +143,5 @@ $(document).ready(function () {
       $('#create-tweet textarea').trigger('focus')
       newTweetIsHidden = true;
     } */
-  })
-})
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
-$(document).ready(function () {
-    console.log('Doc Ready');
+$(document).ready(function() {
+  console.log('Doc Ready');
 
-    $('#tweet-text').on('input', function () {
-        const textCounter = $(`output[for='${this.id}']`);
-        //target the output tag with for attr of this.id and set it to 140 minus string length
-        textCounter.text(140 - this.value.length);
+  $('#tweet-text').on('input', function() {
+    const textCounter = $(`output[for='${this.id}']`);
+    //target the output tag with for attr of this.id and set it to 140 minus string length
+    textCounter.text(140 - this.value.length);
 
-        //check if the content of the textCounter is less than 0 (over 140) and add a class that changes to red if so
-        if(textCounter.text() < 0) {
-            textCounter.addClass('over-limit')
-        } else if(textCounter.hasClass('over-limit')){
-            textCounter.removeClass('over-limit')
-        }
-    })
+    //check if the content of the textCounter is less than 0 (over 140) and add a class that changes to red if so
+    if (textCounter.text() < 0) {
+      textCounter.addClass('over-limit');
+    } else if (textCounter.hasClass('over-limit')) {
+      textCounter.removeClass('over-limit');
+    }
+  });
 });
